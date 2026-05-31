f9135cd0e16df711042521ef586006384986da17
Azer Ibrahimli <aibrahimov191@gmail.com>
2026-05-31 18:06:04 +0400

    Initial Playwright project setup

---
+++ playwright.config.js
@@ -0,0 +1,22 @@
+
+import { defineConfig, devices } from '@playwright/test';
+
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  timeout: 40 * 1000,
+  expect: {
+    timeout: 40 * 1000
+  },
+  
+ 
+  use: {
+  
+   browserName: 'firefox',
+  },
+
+});
+
