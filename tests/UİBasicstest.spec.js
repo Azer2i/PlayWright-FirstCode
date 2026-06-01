7ea1d1eedc56ef8b7b5e216c8630039e2ae14ab5
Azer Ibrahimli <aibrahimov191@gmail.com>
2026-06-02 03:48:17 +0400

    new creater

--- tests/UİBasicstest.spec.js
+++ tests/UİBasicstest.spec.js
@@ -1,20 +1,31 @@
-const {test,expect}  = require("@playwright/test")
+const { test, expect } = require("@playwright/test");
 
-test("browser context playwright test",async({browser}) =>{
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-
-});
-test.only("page context playwright test",async({page}) =>{
+test("page context playwright test", async ({ page }) => {
     await page.goto("https://google.com");
     console.log(await page.title());
     await expect(page).toHaveTitle("Google");
-
 });
-test("browser context playwright test",async({browser}) =>{
+
+test.only("browser context playwright test", async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
+
+    const username = page.locator("#username");
+    const password = page.locator("#password");
+    const submitBtn = page.locator("[type='submit']");
+
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 
+    await username.fill("");
+    await username.fill("rahulshettyacademy");
+    await password.fill("Learning@830$3mK2");
+    await submitBtn.click();
+    await page.goto("https://rahulshettyacademy.com/angularpractice/shop");
+    const text1 = await page.locator(".card-body a").nth(0).textContent();
+    const text2 = await page.locator(".card-body a").nth(1).textContent();
+    console.log(text1);
+    console.log(text2);
+    
+    
+    
 });
